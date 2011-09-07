5a9f6747439add4668b83a7740339ae2a7340e20
Sam Bisbee <sam@sbisbee.com>
2011-09-06 21:59:18 -0400

    Support for message prefixes.

--- Logger.php
+++ Logger.php
@@ -1,8 +1,13 @@
 <?php
 class Logger {
-  private static $logFile = "./app.log";
+  private static $logFile = './app.log';
+  private static $prefix;
 
   private static function addEntry($lvl, $msg) {
+    if(self::$prefix) {
+      $msg = self::$prefix . $msg;
+    }
+
     $handle = fopen(self::$logFile, 'a');
     fwrite($handle, sprintf("%s [%s] %s\n", date("r"), $lvl, $msg));
     fclose($handle);
@@ -10,6 +15,17 @@ class Logger {
 
   public static function setFile($f) {
     self::$logFile = $f;
+  }
+
+  public static function setPrefix($prefix) {
+    if($prefix && (!is_string($prefix) || empty($prefix))) {
+      $prefix = null;
+    }
+    else if(!is_string($prefix)) {
+      throw new Exception('Invalid prefix type: must be a string or null.');
+    }
+
+    self::$prefix = $prefix;
   }
 
   public static function error($msg) {
